b02527eb58916a44fbaf7db0c79b69fe394b71cc
Cristian <eighttax11@gmail.com>
2021-03-13 08:58:10 -0600

    Agregamos las migraciones que no tienen llave foránea, también se agregó tabla de Persona y usuario.

---
+++ database/migrations/2021_03_13_144756_create_people_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name');
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('second_surname');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
